3aebe39d29860a50342f49f48b3d1b0d14e14598
LuizAbudi <luizabudi@hotmail.com>
2025-03-22 16:47:47 -0300

    feat: add Home page and service examples

---
+++ src/pages/Home.tsx
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { Profile, profileService } from "../services/ProfileService";
+import LoadingScreen from '@/components/Loading';
+
+export default function Home() {
+
+  const { data: profile, isLoading, isError,  } = useQuery<Profile | undefined>({
+    queryKey: ["profile", 1],
+    queryFn: async () => {
+      const response = await profileService.getProfile(1);
+      console.log("Response recebido:", response);
+      return response;
+    },
+  });
+
+  return (
+    <div>
+      {isLoading ? (
+        <LoadingScreen />
+      ) : isError ? (
+        <div>Erro ao carregar</div>
+      ) : (
+        <div>
+          <h1>{profile?.title}</h1>
+          <p>{profile?.completed ? "Completo" : "Incompleto"}</p>
+        </div>
+      )}
+    </div>
+  );
+}
