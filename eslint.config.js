db1ad0f234aa0478b8046c57c6ab866d1a7f3d1a
LuizAbudi <luizabudi@hotmail.com>
2025-03-23 20:45:52 -0300

    chore: update ESLint configuration to ignore additional directories and enforce no-console and no-unused-vars rules

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'src/components/ui'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
@@ -22,6 +22,11 @@ export default tseslint.config(
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
+      ],
+      "no-console": "error",
+      "no-unused-vars": [
+        "error",
+        { "argsIgnorePattern": "^_" }
       ],
     },
   },
