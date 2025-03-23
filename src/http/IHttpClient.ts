6f57e2a2bf03fa2e3309a38f360b7002276e0255
LuizAbudi <luizabudi@hotmail.com>
2025-03-23 20:47:23 -0300

    refactor: rename parameters in IHttpClient interface to avoid lint rule

--- src/http/IHttpClient.ts
+++ src/http/IHttpClient.ts
@@ -1,10 +1,10 @@
 import { AxiosRequestConfig } from 'axios';
 
 export interface IHttpClient {
-  get<T = unknown>(url: string, config?: AxiosRequestConfig): Promise<T>;
-  post<T = unknown, D = Record<string, unknown>>(url: string, data?: D, config?: AxiosRequestConfig): Promise<T>;
-  put<T = unknown, D = Record<string, unknown>>(url: string, data?: D, config?: AxiosRequestConfig): Promise<T>;
-  delete<T = unknown>(url: string, config?: AxiosRequestConfig): Promise<T>;
-  patch<T = unknown, D = Record<string, unknown>>(url: string, data?: D, config?: AxiosRequestConfig): Promise<T>;
-  request<T = unknown>(config: AxiosRequestConfig): Promise<T>;
+  get<T = unknown>(__url: string, _config?: AxiosRequestConfig): Promise<T>;
+  post<T = unknown, D = Record<string, unknown>>(_url: string, _data?: D, _config?: AxiosRequestConfig): Promise<T>;
+  put<T = unknown, D = Record<string, unknown>>(_url: string, _data?: D, _config?: AxiosRequestConfig): Promise<T>;
+  delete<T = unknown>(_url: string, _config?: AxiosRequestConfig): Promise<T>;
+  patch<T = unknown, D = Record<string, unknown>>(_url: string, _data?: D, _config?: AxiosRequestConfig): Promise<T>;
+  request<T = unknown>(_config: AxiosRequestConfig): Promise<T>;
 }
