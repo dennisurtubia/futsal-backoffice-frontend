39fa4f5bd6b0ab0fdf669a53ece3274da833b3fc
LuizAbudi <luizabudi@hotmail.com>
2025-03-24 00:06:01 -0300

    feat: add theme provider and switch component for dark/light mode support

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import Home from './pages/Home';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ThemeProvider } from './providers/ThemeProvider';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <div className="flex flex-col items-center justify-center min-h-svh">
-        <Home />
-      </div>
-    </QueryClientProvider>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <div className="flex flex-col items-center justify-center min-h-svh">
+          <Home />
+        </div>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
 
